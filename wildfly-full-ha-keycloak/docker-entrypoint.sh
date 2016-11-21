0585fbaab964d77267d54298287c161dd4b558b5
Martin Večeřa <marvenec@gmail.com>
2016-11-21 23:30:17 +0100

    added wildfly full ha with keycloak adapter

---
+++ wildfly-full-ha-keycloak/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $WILDFLY_USER ] && [ $WILDFLY_PASSWORD ]; then
+    wildfly/bin/add-user.sh --user $WILDFLY_USER --password $WILDFLY_PASSWORD
+fi
+
+IPADDR=$(hostname -i)
+
+echo IP Address: $IPADDR
+
+exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.bind.address=$IPADDR -Djboss.bind.address.private=$IPADDR -Djboss.bind.address.management=$IPADDR -Djboss.node.name=server-$IPADDR $@
+exit $?
