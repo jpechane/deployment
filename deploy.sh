49930549a44bac8f2c01e6e068e816fb33514aec
Martin Večeřa <marvenec@gmail.com>
2016-12-26 23:07:13 +0000

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 cd ~/engine
 git reset --hard HEAD
 git pull
-mvn clean install -DskipTests
+mvn clean install -Pproduction -DskipTests
 
 cd ~/web-ui
 git reset --hard HEAD
 git pull
-sed -i -e 's/"disabled": "true"/"disabled": "false"/' src/main/webapp/WEB-INF
+sed -i -e 's/"disabled": "true"/"disabled": "false"/' src/main/webapp/WEB-INF/keycloak.json
 mvn clean package -DskipTests
 
 cd ~/deployment/engine
