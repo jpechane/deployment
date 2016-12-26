d56053cc1b5f02d23f5547a6656842a2f2d39f33
Martin Večeřa <marvenec@gmail.com>
2016-12-26 22:35:18 +0000

    added ui to engine deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 cd ~/engine
+git reset --hard HEAD
+git pull
 mvn clean install -DskipTests
 
 cd ~/web-ui
+git reset --hard HEAD
+git pull
+sed -i -e 's/"disabled": "true"/"disabled": "false"/' src/main/webapp/WEB-INF
 mvn clean package -DskipTests
+
+cd ~/deployment/engine
+cp ~/engine/war/target/engine-war.war .
+cp ~/web-ui/target/lumeer-web.war .
 
