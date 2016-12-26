cdccbf780733f5edcd2032a9cf1795e03708cfbe
Martin Večeřa <marvenec@gmail.com>
2016-12-27 00:06:50 +0100

    lumeer httpd

--- httpd-keycloak/build.sh
+++ httpd-keycloak/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t lumeer/httpd .
+docker build -t lumeer/httpd-keycloak .
--- httpd-keycloak/start.sh
+++ httpd-keycloak/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -idt -p 80:80 -p 443:443 --name httpd --network lumeer-net lumeer/httpd
+docker run -idt -p 80:80 -p 443:443 --name httpd-keycloak --network keycloak-net lumeer/httpd-keycloak
