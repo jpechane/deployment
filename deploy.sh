ae3453b55b375e76bbbad50e4cb05030e069fe0e
Martin Večeřa <marvenec@gmail.com>
2016-12-27 00:20:44 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,14 @@ git pull
 sed -i -e 's/"disabled": "true"/"disabled": "false"/' src/main/webapp/WEB-INF/keycloak.json
 mvn clean package -DskipTests
 
+if [ ! -f ~/engine/war/target/engine-war.war ]; then
+  exit 1
+fi
+
+if [ ! -f ~/web-ui/target/lumeer-web.war ]; then
+  exit 2
+fi
+
 cd ~/deployment/engine
 cp ~/engine/war/target/engine-war.war .
 cp ~/web-ui/target/lumeer-web.war .
