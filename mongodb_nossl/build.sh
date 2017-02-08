b8bbe256b812be54515ab5c250018ed16a2d7595
Martin Večeřa <marvenec@gmail.com>
2017-02-08 21:26:57 +0100

    mongo without ssl

---
+++ mongodb_nossl/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t lumeer/mongodb_nossl .
---
+++ mongodb_nossl/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -idt -p 27017:27017 --name mongodb --net=lumeer-net -e MONGODB_USER=lumeer -e MONGODB_PASS=/Lumeer1 -e MONGODB_DATABASE=lumeer-test lumeer/mongodb_nossl
