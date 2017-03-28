d4e2854e6cdf51987a3b1ea9317b20fc8aa02362
Martin Večeřa <marvenec@gmail.com>
2017-03-28 21:57:10 +0200

    auto redeploy script #LMR-37

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-cd ~/engine
+cd /home/mvecera/engine
 git reset --hard HEAD
 git pull
 mvn clean install -Pproduction -DskipTests
 
-cd ~/web-ui
+cd /home/mvecera/web-ui
 git reset --hard HEAD
 git pull
 #sed -i -e 's/"disabled": "true"/"disabled": "false"/' src/main/webapp/WEB-INF/keycloak.json
 mvn clean package -DskipTests
 
-if [ ! -f ~/engine/war/target/engine-war.war ]; then
+if [ ! -f /home/mvecera/engine/war/target/engine-war.war ]; then
   exit 1
 fi
 
-if [ ! -f ~/web-ui/target/lumeer-web.war ]; then
+if [ ! -f /home/mvecera/web-ui/target/lumeer-web.war ]; then
   exit 2
 fi
 
-cd ~/deployment/engine
-cp ~/engine/war/target/engine-war.war .
-cp ~/web-ui/target/lumeer-web.war .
+cd /home/mvecera/deployment/engine
+cp /home/mvecera/engine/war/target/engine-war.war .
+cp /home/mvecera/web-ui/target/lumeer-web.war .
+
+/home/mvecera/deployment/engine/restart-v10.sh
 
