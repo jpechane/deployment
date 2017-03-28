41c6b5160666172eeaa0fd1f823090d1f1a7ae5e
Martin Večeřa <marvenec@gmail.com>
2017-03-28 19:28:54 +0000

    cert extraction

--- httpd-keycloak/renew-auth.sh
+++ httpd-keycloak/renew-auth.sh
@@ -2,3 +2,5 @@
 docker stop httpd-keycloak
 certbot-auto renew
 docker start httpd-keycloak
+openssl x509 -outform der -in /etc/letsencrypt/live/auth.lumeer.io/cert.pem -out /mnt/letsencrypt/certificate.der
+keytool -noprompt -v -import -file /mnt/letsencrypt/certificate.der -alias auth.lumeer.io -keystore /mnt/letsencrypt/cacerts.jks -storepass geheim
